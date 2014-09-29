d5e4ee97e1650dad4ba67c1ecd01a0bbf638fdf4
Mitch Garnaat <mitch@scopely.com>
2014-09-29 06:45:09 -0700

    Changing copyright notice. Removing NewRelic stuff for now.

--- skew/arn/endpoint.py
+++ skew/arn/endpoint.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Mitch Garnaat
+# Copyright 2014 Scopely, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
