8d363adfe5fd3654fb935cc7f85aa3e293135585
EmmyTee <Ricky@Rickys-Computer.local>
2017-06-14 17:21:08 +0100

    Region Update
    Added Ohio, London, Sao Paulo, and Montreal regions also updated
    regions for the service regions map.

--- skew/arn/__init__.py
+++ skew/arn/__init__.py
@@ -150,14 +150,18 @@ class Account(ARNComponent):
 
 class Region(ARNComponent):
     _all_region_names = ['us-east-1',
+                         'us-east-2',
                          'us-west-1',
                          'us-west-2',
                          'eu-west-1',
+                         'eu-west-2',
                          'eu-central-1',
                          'ap-southeast-1',
                          'ap-southeast-2',
                          'ap-northeast-1',
                          'ap-northeast-2',
+                         'ap-south-1',
+                         'ca-central-1',
                          'sa-east-1']
 
     _region_names_limited = ['us-east-1',
@@ -170,13 +174,15 @@ class Region(ARNComponent):
     _no_region_required = ['']
 
     _service_region_map = {
-        'redshift': _region_names_limited,
-        'glacier': _region_names_limited,
+        'redshift': _all_region_names,
+        'glacier': ['ap-northeast-1', 'ap-northeast-2', 'ap-south-1', 'ap-southeast-2', 'ca-central-1', 'eu-central-1',
+                    'eu-west-1', 'eu-west-2', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'],
         'kinesis': _region_names_limited,
         'cloudfront': _no_region_required,
         'iam': _no_region_required,
         'route53': _no_region_required,
-        'lambda': ['us-east-1', 'us-west-2', 'eu-west-1', 'ap-northeast-1'],
+        'lambda': ['ap-northeast-1', 'ap-northeast-2', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-central-1',
+                   'eu-west-1', 'eu-west-2', 'sa-east-1', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'],
         'firehose': ['us-east-1', 'us-west-2', 'eu-west-1'],
         'apigateway': ['us-east-1', 'us-west-2', 'eu-west-1', 'ap-northeast-1'],
     }
