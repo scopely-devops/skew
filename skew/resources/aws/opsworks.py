72a7f837c4ba4a2e109411fea56634a0b36ee8e1
Christophe Morio <c.morio@linkbynet.com>
2018-10-23 10:46:38 +0200

    Add OpsWorks stack

---
+++ skew/resources/aws/opsworks.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2014 Scopely, Inc.
+# Copyright (c) 2015 Mitch Garnaat
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import logging
+
+from skew.resources.aws import AWSResource
+
+
+LOG = logging.getLogger(__name__)
+
+
+class Stack(AWSResource):
+
+    class Meta(object):
+        service = 'opsworks'
+        type = 'stack'
+        resourcegroups_tagging = True # only support regional, not classic (us-east-1)
+        enum_spec = ('describe_stacks', 'Stacks', None)
+        detail_spec = None
+        id = 'StackId'
+        filter_name = None
+        name = 'Name'
+        date = 'CreatedAt'
+        dimension = None
+        tags_spec = ('list_tags', 'Tags',
+                     'ResourceArn', 'arn')
+
+    @property
+    def arn(self):
+        return self.data.get('Arn')
