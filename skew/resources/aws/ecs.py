94298a224eeaaa7908feb4ca9776095159faf5f4
Christophe Morio <c.morio@linkbynet.com>
2018-12-26 09:20:49 +0100

    Add tagging support for ECS

--- skew/resources/aws/ecs.py
+++ skew/resources/aws/ecs.py
@@ -31,7 +31,8 @@ class Cluster(AWSResource):
         enum_spec = ('list_clusters', 'clusterArns', None)
         detail_spec = ('describe_clusters', 'clusters', 'clusters[0]')
         id = None
-
+        tags_spec = ('list_tags_for_resource', 'tags[]',
+                     'resourceArn', 'arn')
 
         filter_name = None
         name = 'clusterName'
