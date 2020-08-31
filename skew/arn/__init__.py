61fb05c716c9df2a9332f895cfd1a148315c6517
tobhai <t.haindl@gmx.net>
2020-08-31 09:45:48 +0200

    Firehose is now available in every region except `ap-northeast-3` which is not included in regions because it is a special local region

--- skew/arn/__init__.py
+++ skew/arn/__init__.py
@@ -183,7 +183,6 @@ class Region(ARNComponent):
         'route53': _no_region_required,
         'lambda': ['ap-northeast-1', 'ap-northeast-2', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-central-1',
                    'eu-west-1', 'eu-west-2', 'sa-east-1', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'],
-        'firehose': ['us-east-1', 'us-west-2', 'eu-west-1'],
         'apigateway': ['us-east-1', 'us-west-2', 'eu-west-1', 'ap-northeast-1'],
     }
 
