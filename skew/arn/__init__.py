f106a50e17167823935722d40e3c5b5ece92cae2
tobhai <t.haindl@gmx.net>
2020-08-31 09:47:20 +0200

    Add new regions

--- skew/arn/__init__.py
+++ skew/arn/__init__.py
@@ -155,14 +155,22 @@ class Region(ARNComponent):
                          'us-west-2',
                          'eu-west-1',
                          'eu-west-2',
+                         'eu-west-3',
                          'eu-central-1',
+                         'eu-north-1',
+                         'eu-south-1',
                          'ap-southeast-1',
                          'ap-southeast-2',
                          'ap-northeast-1',
                          'ap-northeast-2',
                          'ap-south-1',
+                         'ap-east-1',
+                         'af-south-1'
                          'ca-central-1',
-                         'sa-east-1']
+                         'sa-east-1',
+                         'me-south-1',
+                         'cn-north-1',
+                         'cn-northwest-1']
 
     _region_names_limited = ['us-east-1',
                              'us-west-2',
