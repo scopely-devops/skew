9c8cee0f3ad02022b4df1e0f7fa0626f153ffc97
tobhai <t.haindl@gmx.net>
2020-08-31 09:47:07 +0200

    Lambda is now available in every region except `ap-northeast-3` which is not included in regions because it is a special local region

--- skew/arn/__init__.py
+++ skew/arn/__init__.py
@@ -180,9 +180,7 @@ class Region(ARNComponent):
         'kinesis': _region_names_limited,
         'cloudfront': _no_region_required,
         'iam': _no_region_required,
-        'route53': _no_region_required,
-        'lambda': ['ap-northeast-1', 'ap-northeast-2', 'ap-south-1', 'ap-southeast-1', 'ap-southeast-2', 'eu-central-1',
-                   'eu-west-1', 'eu-west-2', 'sa-east-1', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'],
+        'route53': _no_region_required
     }
 
     def choices(self, context=None):
